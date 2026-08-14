4a26ad2dd2cf82674acfe6611d364c523041ee93
Reham Hesham <rehamhesham7777@gmail.com>
2026-08-14 13:33:22 +0300

    stage 3 - Switch Statement

---
+++ src/main/java/stage3.java
@@ -0,0 +1,27 @@
+public class stage3 {
+    public static void main(String[] args) {
+
+        int departmentCODE = 1 ;
+
+        switch (departmentCODE) {
+            case 1 :
+                System.out.println("Your Department is IT");
+                break;
+            case 2 :
+                System.out.println("Your Department is HR");
+                break;
+            case 3 :
+                System.out.println("Your Department is Finance");
+                break;
+            case 4 :
+                System.out.println("Your Department is Sales");
+                break;
+            case 5 :
+                System.out.println("Your Department is Marketing");
+                break;
+            default:
+                System.out.println("Unknown Department");
+
+        }
+    }
+}
