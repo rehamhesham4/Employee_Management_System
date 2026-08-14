6e28d055d29bb28c296123dfb37ff2a7ab6cfc4c
Reham Hesham <rehamhesham7777@gmail.com>
2026-08-14 13:03:10 +0300

    stage 2 - If/Else

--- src/main/java/stage1.java
+++ src/main/java/stage1.java
@@ -4,35 +4,42 @@ public class stage1 {
         int EmployeeID = 1001;
         String EmployeeName = ("Reham Hesham");
         String Department = ("IT");
-        double BasicSalary = 5000;
-        double Bonus = 2500;
+        double BasicSalary = 15000;
+        double Bonus = 3000;
         boolean Married = false;
-        //----------------------------------
-            // gross salary
-            int x = 5000;
-            int y = 2500;
-            int z = x + y;
+  //----------------------------------
 
-           //tax percentage
-            double p = .20;
-            double t = z * p;
+        // gross salary
+        int x = 15000;
+        int y = 3000;
+        int z = x + y;
+
+        //tax percentage
+        double p = .0989;
+        double t = z * p;
 
 
-            //net salary
-           double n = z - t;
+        //net salary
+        double n = z - t;
 
-       //-----------------------------------------
+  //-----------------------------------------
         System.out.println("EmployeeID: " + EmployeeID);
         System.out.println("EmployeeName: " + EmployeeName);
-        System.out.println("Department: " +Department );
+        System.out.println("Department: " + Department);
         System.out.println("BasicSalary: " + BasicSalary);
-        System.out.println("Bonus: "+ Bonus);
-        System.out.println("Married: " + Married );
-        System.out.println("Gross Salary = " +z);
-        System.out.println("Tax Amont = " +t);
-        System.out.println("Net Salary = " +n);
-    }
+        System.out.println("Bonus: " + Bonus);
+        System.out.println("Married: " + Married);
+        System.out.println("Gross Salary = " + z);
+        System.out.println("Tax Amount = " + t);
+        System.out.println("Net Salary = " + n);
 
+
+//-------------------------------------------------
+
+
+
+
+    }
 }
 
 
---
+++ src/main/java/stage2.java
@@ -0,0 +1,44 @@
+public class stage2 {
+    public static void main(String[] args) {
+
+        double netSalary = 16219.8;
+        boolean Married = false;
+
+
+
+
+        if (netSalary >= 30000.) {
+            System.out.println("Income Level : High Income");
+
+        }else if (netSalary >= 15000.){
+            System.out.println("Income Level : Medium Income");
+        }else if (netSalary > 15000.) {
+
+            System.out.println("Income Level : Low Income");
+        }
+
+
+//--------------------------------------------------------
+
+        if (Married){
+
+            System.out.println("Family Insurance : Eligible");
+
+        }else {
+
+            System.out.println("Family Insurance : Not Eligible");
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
